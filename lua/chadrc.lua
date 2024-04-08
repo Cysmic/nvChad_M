89e66a80e5688f5f83195b1af7ea8b1f24a56d98
Billy Flare <hassassin117@gmail.com>
2024-04-08 17:36:40 -0500

    uses terminal background now. included some backgrounds to use

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -4,10 +4,10 @@ local M = {}
 M.ui = {
   theme = "gatekeeper",
 
-  -- hl_override = {
-  --   Comment = { italic = true },
-  --   ["@comment"] = { italic = true },
-  -- },
+  hl_override = {
+      Pmenu = { bg = "white" },
+      Normal = { bg = "NONE" },
+  },
 }
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -7,5 +7,10 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     command = "set filetype=glsl",
 })
 
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+    vim.o.fileformat = 'dos'
+else
+    vim.o.fileformat = 'unix'
+end
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
